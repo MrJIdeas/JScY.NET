faaa3ac9021c77f66bd11e84dc317b25486f39b2
Michael Joppe <michael@JOPPE.intern>
2023-12-23 18:15:21 +0100

    Update .NET 8.0

--- ScienceTest/Program.cs
+++ ScienceTest/Program.cs
@@ -1,12 +1,10 @@
-﻿using Python.Runtime;
+﻿using JScience.Physik.Enums;
+using JScience.Physik.Simulationen.Spins.Classic.Simulations;
+using JScience.Physik.Simulationen.Spins.Enums;
 
-//ANNNI_Classic_1D test = new ANNNI_Classic_1D(-1, 0.3, 0, 1, 10, EParticleType.ZBoson, ELatticeBoundary.Periodic, 1);
-//test.Start();
-
-//while (test.n < 1000) ;
-//test.Stop();
-PythonEngine.Initialize();
-using (Py.GIL())
-{
-    PythonEngine.Exec("a=1");
-}
+ANNNI_Classic_1D test = new ANNNI_Classic_1D(-1, 0.3, 0, 1, 10, EParticleType.ZBoson, ELatticeBoundary.Periodic, 1);
+Console.WriteLine(test.H());
+test.Start();
+while (test.n < 1000) ;
+test.Stop();
+Console.WriteLine(test.H());
