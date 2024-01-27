affef019f7cd4567c455e8defec9d243d5183b9c
Michael Joppe <michael@JOPPE.intern>
2024-01-27 13:45:45 +0100

    Zeitentwicklungsoperator Interface sowie rekursive Implementierung 1D

---
+++ JScience/Physik/Simulationen/Wavefunctions/TimeEvolution/Classes/U_T_1D.cs
@@ -0,0 +1,32 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.Classes
+{
+    internal class U_T_1D<T> : IU_T<T> where T : IWF_1D
+    {
+        public double t_STEP { get; private set; }
+
+        public T Do(T WF, List<IHamilton<T>> Hamiltons)
+        {
+            T WFp1 = (T)Activator.CreateInstance(typeof(T), true, WF.DimX);
+            foreach (var ham in Hamiltons)
+            {
+                var hpsi = ham.HPsi(WF);
+                if (hpsi.Norm() < double.Epsilon)
+                    Hamiltons.Remove(ham);
+                else
+                    WFp1 = (T)(hpsi + WFp1);
+            }
+            T erg = (T)(-Complex.ImaginaryOne * t_STEP * (WF + WFp1));
+            if (WFp1.Norm() < double.Epsilon)
+                return erg;
+            else
+                return Do(erg, Hamiltons);
+        }
+    }
+}
---
+++ JScience/Physik/Simulationen/Wavefunctions/TimeEvolution/Interfaces/IU_T.cs
@@ -0,0 +1,15 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.Interfaces
+{
+    public interface IU_T<T> where T : IWavefunction
+    {
+        double t_STEP { get; }
+
+        T Do(T WF, List<IHamilton<T>> Hamiltons);
+    }
+}
