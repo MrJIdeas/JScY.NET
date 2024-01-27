f2aa3e734a2af0accc453fcfc1d2b4a2fc3b8ada
Michael Joppe <michael@JOPPE.intern>
2024-01-27 11:56:44 +0100

    Interface Hamiltonoperator

---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/Interfaces/IHamilton.cs
@@ -0,0 +1,11 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces
+{
+    public interface IHamilton<T> where T : IWavefunction
+    {
+        T HPsi(T psi);
+
+        double E(T psi);
+    }
+}
---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/TightBinding/Interfaces/ITightBinding.cs
@@ -0,0 +1,9 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.TightBinding.Interfaces
+{
+    public interface ITightBinding<T> : IHamilton<T> where T : IWavefunction
+    {
+    }
+}
