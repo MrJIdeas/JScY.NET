aedad2cefbb091f374a553697a6e8499d7789bf4
Michael Joppe <michael@JOPPE.intern>
2024-01-27 13:45:21 +0100

    Tight Binding Operatoren

--- JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/TightBinding/Interfaces/ITightBinding.cs
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/TightBinding/Interfaces/ITightBinding.cs
@@ -1,9 +1,15 @@
 ﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
 using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using System;
 
 namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.TightBinding.Interfaces
 {
-    public interface ITightBinding<T> : IHamilton<T> where T : IWavefunction
+    public abstract class ITightBinding<T> : IHamilton<T> where T : IWavefunction
     {
+        public double t_Hopping { get; }
+
+        public double E(T psi) => throw new NotImplementedException();
+
+        public T HPsi(T psi) => throw new NotImplementedException();
     }
 }
---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/TightBinding/VarTypes/TightBindung2D.cs
@@ -0,0 +1,22 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Enums;
+using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.TightBinding.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.TightBinding.VarTypes
+{
+    public class TightBindung2D<T> : ITightBinding<T> where T : IWF_2D
+    {
+        public new double E(T psi)
+        {
+            double erg = 0;
+            T erg2 = (T)((T)psi.Conj() * HPsi(psi));
+            for (int i = 0; i < erg2.DimX; i++)
+                for (int j = 0; j < erg2.DimY; j++)
+                    erg += erg2[i, j].Magnitude;
+            return erg;
+        }
+
+        public new T HPsi(T psi) => (T)(-t_Hopping * (T)((T)psi.GetShift(EShift.Xm) + (T)psi.GetShift(EShift.Xp))
+                                                            + (T)psi.GetShift(EShift.Ym) + (T)psi.GetShift(EShift.Yp));
+    }
+}
