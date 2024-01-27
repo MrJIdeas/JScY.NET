2ad1581ada58e1941aff970e5cd4b5d7fff9d7db
Michael Joppe <michael@JOPPE.intern>
2024-01-27 21:38:00 +0100

    Implementierung Imaginäre Potentiale 1D und 2D

---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/ImaginaryPotential1D.cs
@@ -0,0 +1,37 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using System;
+using System.Numerics;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes
+{
+    public class ImaginaryPotential1D<T> : IImagenaryPotential<T> where T : IWF_1D
+    {
+        public int xStart { get; private set; }
+
+        public int xEnd { get; private set; }
+
+        public ImaginaryPotential1D(int xSTART, int xEND, double damping) : base(damping)
+        {
+            xStart = xSTART;
+            xEnd = xEND;
+        }
+
+        public override double E(T psi)
+        {
+            double erg = 0;
+            T erg2 = (T)((T)psi.Conj() * HPsi(psi));
+            for (int i = 0; i < erg2.DimX; i++)
+                erg += (Complex.Conjugate(erg2[i]) * erg2[i]).Real;
+            return erg;
+        }
+
+        public override T HPsi(T psi)
+        {
+            T psiV = (T)Activator.CreateInstance(psi.GetType(), psi.DimX);
+            for (int i = xStart; i < xEnd; i++)
+                psiV.SetField(i, Damping * psi[i]);
+            return psiV;
+        }
+    }
+}
---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/ImaginaryPotential2D.cs
@@ -0,0 +1,45 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using System;
+using System.Numerics;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes
+{
+    public class ImaginaryPotential2D<T> : IImagenaryPotential<T> where T : IWF_2D
+    {
+        public int xStart { get; private set; }
+
+        public int xEnd { get; private set; }
+
+        public int yStart { get; private set; }
+
+        public int yEnd { get; private set; }
+
+        public ImaginaryPotential2D(int xSTART, int xEND, int ySTART, int yEND, double damping) : base(damping)
+        {
+            xStart = xSTART;
+            xEnd = xEND;
+            yStart = ySTART;
+            yEnd = yEND;
+        }
+
+        public override double E(T psi)
+        {
+            double erg = 0;
+            T erg2 = (T)((T)psi.Conj() * HPsi(psi));
+            for (int i = 0; i < erg2.DimX; i++)
+                for (int j = 0; j < erg2.DimY; j++)
+                    erg += (Complex.Conjugate(erg2[i, j]) * erg2[i, j]).Real;
+            return erg;
+        }
+
+        public override T HPsi(T psi)
+        {
+            T psiV = (T)Activator.CreateInstance(psi.GetType(), psi.DimX);
+            for (int i = xStart; i < xEnd; i++)
+                for (int j = yStart; j < yEnd; j++)
+                    psiV.SetField(i, j, Damping * psi[i, j]);
+            return psiV;
+        }
+    }
+}
