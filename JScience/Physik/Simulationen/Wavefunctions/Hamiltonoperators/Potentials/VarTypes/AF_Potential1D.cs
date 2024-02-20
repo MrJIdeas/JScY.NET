66b38d4486ca82e321ae38e32873646c60af8bf3
Michael Joppe <michael@JOPPE.intern>
2024-02-20 15:34:04 +0100

    Antiferromagnetische Potentiale

---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/AF_Potential1D.cs
@@ -0,0 +1,29 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using System;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes
+{
+    public class AF_Potential1D<T> : Potential1D<T>, IAFBarrier where T : IWF_1D
+    {
+        public AF_Potential1D(string name, int xSTART, int xEND, decimal Vmax, int Blocksize) : base(name, xSTART, xEND, Vmax)
+        {
+            this.Blocksize = Blocksize;
+        }
+
+        public int Blocksize { get; private set; }
+
+        public override T HPsi(T psi)
+        {
+            T psiV = (T)Activator.CreateInstance(psi.GetType(), psi.DimX, psi.Boundary);
+            for (int i = xStart; i < xEnd; i++)
+            {
+                if ((i - xStart) % Blocksize % 2 == 0)
+                    psiV.SetField(i, Potential * psi[i]);
+                else
+                    psiV.SetField(i, -Potential * psi[i]);
+            }
+            return psiV;
+        }
+    }
+}
---
+++ JScience/Physik/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/AF_Potential2D.cs
@@ -0,0 +1,28 @@
+﻿using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using System;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes
+{
+    public class AF_Potential2D<T> : Potential2D<T>, IAFBarrier where T : IWF_2D
+    {
+        public AF_Potential2D(string name, int xSTART, int ySTART, int xEND, int yEND, decimal Vmax, int blocksize) : base(name, xSTART, ySTART, xEND, yEND, Vmax)
+        {
+            Blocksize = blocksize;
+        }
+
+        public int Blocksize { get; private set; }
+
+        public override T HPsi(T psi)
+        {
+            T psiV = (T)Activator.CreateInstance(psi.GetType(), psi.DimX, psi.DimY, psi.Boundary);
+            for (int i = xStart; i < xEnd; i++)
+                for (int j = yStart; j < yEnd; j++)
+                    if ((i - xStart) % Blocksize % 2 == 0 && (j - yStart) % Blocksize % 2 == 1)
+                        psiV.SetField(i, j, Potential * psi[i, j]);
+                    else
+                        psiV.SetField(i, j, Potential * psi[i, j]);
+            return psiV;
+        }
+    }
+}
