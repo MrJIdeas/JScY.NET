6b4262012c255e08580af6db6179eae402d7970a
Michael Joppe <michael@JOPPE.intern>
2024-02-20 14:39:56 +0100

    Beispiele angepasst

--- ScienceTest/Program.cs
+++ ScienceTest/Program.cs
@@ -10,6 +10,7 @@ using JScience.Physik.Simulationen.Wavefunctions.VarTypes.StandardWF;
 using SkiaSharp;
 using System.Drawing;
 using System.Drawing.Imaging;
+using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes;
 
 //Ising_Classic_1D_Lattice test = new Ising_Classic_1D_Lattice(-1, 0, 0, 6, 10000, EParticleType.ZBoson, ELatticeBoundary.Periodic, 1);
 //Console.WriteLine(test.H());
@@ -17,26 +18,41 @@ using System.Drawing.Imaging;
 //Console.WriteLine(test.H());
 //Console.WriteLine(test.n);
 
-//WF_1D test = (WF_1D)WFCreator.CreateGaußWave(1, 25, 500, 250, ELatticeBoundary.Periodic);
-//WF_2D test = (WF_2D)WFCreator.CreateGaußWave(1, 0, 25, 25, 100, 100, 50, 50, ELatticeBoundary.Periodic);
+WF_1D test = (WF_1D)WFCreator.CreateGaußWave(-5, 10, 100, 25, ELatticeBoundary.Reflection);
 //WF_1D test = (WF_1D)WFCreator.CreateFreeWave(1, 100, ELatticeBoundary.Periodic);
+//WF_1D test = (WF_1D)WFCreator.CreateDelta(100, 50, ELatticeBoundary.Periodic);
+//WF_2D test = (WF_2D)WFCreator.CreateGaußWave(-1, 0, 25, 25, 100, 100, 25, 25, ELatticeBoundary.Reflection);
 //WF_2D test = (WF_2D)WFCreator.CreateFreeWave(1, 0, 100, 100, 50, 50, ELatticeBoundary.Reflection);
-WF_1D test = (WF_1D)WFCreator.CreateDelta(100, 50, ELatticeBoundary.Periodic);
-//WF_2D test = (WF_2D)WFCreator.CreateDelta(100, 100, 25, 25, ELatticeBoundary.Periodic);
+//WF_2D test = (WF_2D)WFCreator.CreateDelta(100, 100, 50, 50, ELatticeBoundary.Reflection);
 Console.WriteLine("Norm: " + test.Norm());
 
 List<IHamilton<WF_1D>> hamlist = new List<IHamilton<WF_1D>>();
 
 TightBindung1D<WF_1D> ham = new TightBindung1D<WF_1D>(1);
+Potential1D<WF_1D> pot1 = new Potential1D<WF_1D>("PotMitte", 40 * test.DimX / 100, 60 * test.DimX / 100, 1);
+ImaginaryPotential1D<WF_1D> imagpotl = new ImaginaryPotential1D<WF_1D>("ImagPotLinks", 0, test.DimX / 100, 100);
+ImaginaryPotential1D<WF_1D> imagpotr = new ImaginaryPotential1D<WF_1D>("ImagPotRechts", 90 * test.DimX / 100, test.DimX, 100);
 
 //List<IHamilton<WF_2D>> hamlist = new List<IHamilton<WF_2D>>();
 
 //TightBindung2D<WF_2D> ham = new TightBindung2D<WF_2D>(1);
+//Potential2D<WF_2D> pot1 = new Potential2D<WF_2D>("PotMitte",  40 * test.DimX/100, 60 * test.DimX/100, 0, test.DimY, 10);
+//Potential2D<WF_2D> pot2 = new Potential2D<WF_2D>("PotMitte", 0, test.DimX, 40 * test.DimY/100, 60 * test.DimY/100, 10);
 
+//ImaginaryPotential2D<WF_2D> imagpotl = new ImaginaryPotential2D<WF_2D>("ImagPotLinks", 0, 5 * test.DimX/100, 0, test.DimY, 10);
+//ImaginaryPotential2D<WF_2D> imagpotr = new ImaginaryPotential2D<WF_2D>("ImagPotRechts", 95 * test.DimX/100, test.DimX, 0, test.DimY, 10);
+//ImaginaryPotential2D<WF_2D> imagpoto = new ImaginaryPotential2D<WF_2D>("ImagPotLinks", 0, test.DimX, 95 * test.DimY/100, test.DimY, 10);
+//ImaginaryPotential2D<WF_2D> imagpotu = new ImaginaryPotential2D<WF_2D>("ImagPotRechts", 0, test.DimX, 0, 5 * test.DimY/100, 10);
 hamlist.Add(ham);
+hamlist.Add(imagpotl);
+hamlist.Add(imagpotr);
+//hamlist.Add(imagpoto);
+//hamlist.Add(imagpotu);
+hamlist.Add(pot1);
+//hamlist.Add(pot2);
 
 U_T_1D<WF_1D> ze = new U_T_1D<WF_1D>(0.5m);
-//U_T_2D<WF_2D> ze = new U_T_2D<WF_2D>(0.5);
+//U_T_2D<WF_2D> ze = new U_T_2D<WF_2D>(0.5m);
 
 FFmpegLoader.FFmpegPath = @"C:\ffmpeg\bin\";
 
@@ -55,7 +71,7 @@ var bitmapData = ImageData.FromPointer(bitLock.Scan0, ImagePixelFormat.Bgr24, pi
 file.Video.AddFrame(bitmapData); // Encode the frame
 pic.UnlockBits(bitLock);
 
-for (int i = 0; i < 100; i++)
+for (int i = 0; i < 200; i++)
 {
     DateTime start = DateTime.Now;
     test = ze.Do(test, hamlist);
