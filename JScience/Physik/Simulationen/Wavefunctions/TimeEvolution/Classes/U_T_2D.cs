b2d83a681d1ec66cda3cd5a6b2beae82039699a9
Michael Joppe <michael@JOPPE.intern>
2024-02-25 18:21:07 +0100

    Vereinheitlichung Zeitentwicklungsoperator bis auf Termglied

--- JScience/Physik/Simulationen/Wavefunctions/TimeEvolution/Classes/U_T_2D.cs
+++ JScience/Physik/Simulationen/Wavefunctions/TimeEvolution/Classes/U_T_2D.cs
@@ -2,6 +2,7 @@
 using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
 using JScience.Physik.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
 using JScience.Physik.Simulationen.Wavefunctions.Interfaces;
+using JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.BaseClasses;
 using JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,32 +10,13 @@ using System.Linq;
 
 namespace JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.Classes
 {
-    public class U_T_2D<T> : IU_T<T> where T : IWF_2D
+    public class U_T_2D<T> : U_T_Base<T> where T : IWF_2D
     {
-        public U_T_2D(decimal t_step)
+        public U_T_2D(decimal t_step) : base(t_step)
         {
-            t_STEP = t_step;
         }
 
-        public decimal t_STEP { get; private set; }
-
-        public T Do(T WF, List<IHamilton<T>> Hamiltons)
-        {
-            T WFEnd = (T)WF.Clone();
-            int n = 1;
-            T WF1 = PsiNTerm(WF, Hamiltons, n);
-            WFEnd = (T)(WFEnd + WF1);
-
-            while (WF1.Norm() > 1E-30m)
-            {
-                n++;
-                WF1 = PsiNTerm(WF1, Hamiltons, n);
-                WFEnd = (T)(WFEnd + WF1);
-            }
-            return WFEnd;
-        }
-
-        private T PsiNTerm(T WF, List<IHamilton<T>> Hamiltons, int n)
+        protected override T PsiNTerm(T WF, List<IHamilton<T>> Hamiltons, int n)
         {
             T WF1 = (T)Activator.CreateInstance(WF.GetType(), WF.DimX, WF.DimY, WF.Boundary);
             List<IHamilton<T>> hamtodelete = new List<IHamilton<T>>();
