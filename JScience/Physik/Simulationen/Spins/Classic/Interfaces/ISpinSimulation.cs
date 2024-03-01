7e8eb9a480822f07b2f651e2505f636951f5b5cf
Michael Joppe <michael@JOPPE.intern>
2024-03-01 21:49:21 +0100

    Kommentarergänzung allgemeine Dinge Physik

--- JScience/Physik/Simulationen/Spins/Classic/Interfaces/ISpinSimulation.cs
+++ JScience/Physik/Simulationen/Spins/Classic/Interfaces/ISpinSimulation.cs
@@ -3,16 +3,54 @@ using JScience.Physik.Simulationen.Spins.Enums;
 
 namespace JScience.Physik.Simulationen.Spins.Classic.Interfaces
 {
+    /// <summary>
+    /// Interface für Spinsimulation
+    /// </summary>
     public interface ISpinSimulation
     {
+        /// <summary>
+        /// Magnetische Flussdichte.
+        /// </summary>
         double B { get; }
+
+        /// <summary>
+        /// Bezeichnung der Simulation.
+        /// </summary>
         string Bezeichnung { get; }
+
+        /// <summary>
+        /// Begrenzung.
+        /// </summary>
         ELatticeBoundary Boundary { get; }
+
+        /// <summary>
+        /// Dimensionen.
+        /// </summary>
         uint DimLength { get; }
+
+        /// <summary>
+        /// Kopplungskonstante der Spins.
+        /// </summary>
         double J { get; }
+
+        /// <summary>
+        /// Magnetisierungstyp.
+        /// </summary>
         EMagnetiseType MagType { get; }
+
+        /// <summary>
+        /// Anzahl Spins.
+        /// </summary>
         ulong n { get; }
+
+        /// <summary>
+        /// Partikeltypen.
+        /// </summary>
         EParticleType ParticleType { get; }
+
+        /// <summary>
+        /// Temperatur
+        /// </summary>
         double T { get; }
     }
 }
--- JScience/Physik/Simulationen/Spins/Enums/ELatticeBoundary.cs
+++ JScience/Physik/Simulationen/Spins/Enums/ELatticeBoundary.cs
@@ -1,9 +1,23 @@
 ﻿namespace JScience.Physik.Simulationen.Spins.Enums
 {
+    /// <summary>
+    /// Enum für Gitterbegrenzungen.
+    /// </summary>
     public enum ELatticeBoundary
     {
+        /// <summary>
+        /// Keine Angabe.
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Feste Ränder.
+        /// </summary>
         Reflection = 1,
+
+        /// <summary>
+        /// Periodische Fortsetzung.
+        /// </summary>
         Periodic = 2
     }
 }
