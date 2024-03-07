7d97d84ccd2bdf5369876d0f3160f7714f43bf3d
Michael Joppe <michael@JOPPE.intern>
2024-03-07 22:39:42 +0100

    Beispielanpassung für Cab

--- ScienceTest/Program.cs
+++ ScienceTest/Program.cs
@@ -14,7 +14,7 @@ IWavefunction.InitOpenCL();
 //WF_1D test = (WF_1D)WFCreator.CreateGaußWave(-5, 10, 100, 25, ELatticeBoundary.Reflection, ECalculationMethod.OpenCL);
 //WF_1D test = (WF_1D)WFCreator.CreateFreeWave(1, 100, ELatticeBoundary.Periodic,  ECalculationMethod.OpenCL);
 //WF_1D test = (WF_1D)WFCreator.CreateDelta(100, 50, ELatticeBoundary.Periodic,  ECalculationMethod.OpenCL);
-WF_2D test = (WF_2D)WFCreator.CreateGaußWave(-1, -1, 25, 25, 100, 100, 25, 25, ELatticeBoundary.Reflection, ECalculationMethod.OpenCL);
+WF_2D test = (WF_2D)WFCreator.CreateGaußWave(5, 0, 1, 1, 100, 50, 25, 25, ELatticeBoundary.Reflection, ECalculationMethod.OpenCL);
 //WF_2D test = (WF_2D)WFCreator.CreateFreeWave(1, 0, 100, 100, 50, 50, ELatticeBoundary.Reflection,  ECalculationMethod.OpenCL);
 //WF_2D test = (WF_2D)WFCreator.CreateDelta(100, 100, 50, 50, ELatticeBoundary.Reflection,  ECalculationMethod.OpenCL);
 Console.WriteLine("Norm: " + test.Norm());
@@ -26,32 +26,36 @@ BlockPotential<WF_2D> pot1 = new BlockPotential<WF_2D>("PotMitte", 40 * test.Dim
 BlockPotential<WF_2D> pot2 = new BlockPotential<WF_2D>("PotMitte", 0, 40 * test.DimY / 100, test.DimX, 60 * test.DimY / 100, 0.1);
 AF_Potential<WF_2D> afpot1 = new AF_Potential<WF_2D>("PotMitte", 40 * test.DimX / 100, 0, 60 * test.DimX / 100, test.DimY, 1, 5);
 AF_Potential<WF_2D> afpot2 = new AF_Potential<WF_2D>("PotMitte", 0, 40 * test.DimY / 100, test.DimX, 60 * test.DimY / 100, 1, 5);
-ImaginaryPotential<WF_2D> imagpotl = new ImaginaryPotential<WF_2D>("ImagPotLinks", 0, 3 * test.DimX / 100, 0, test.DimY, 10);
-ImaginaryPotential<WF_2D> imagpotr = new ImaginaryPotential<WF_2D>("ImagPotRechts", 97 * test.DimX / 100, test.DimX, 0, test.DimY, 10);
-ImaginaryPotential<WF_2D> imagpoto = new ImaginaryPotential<WF_2D>("ImagPotLinks", 3 * test.DimX / 100, 97 * test.DimX / 100, 97 * test.DimY / 100, test.DimY, 10);
-ImaginaryPotential<WF_2D> imagpotu = new ImaginaryPotential<WF_2D>("ImagPotRechts", 3 * test.DimX / 100, 97 * test.DimX / 100, 0, 3 * test.DimY / 100, 10);
+ImaginaryPotential<WF_2D> imagpotl = new ImaginaryPotential<WF_2D>("ImagPotLinks", 0, 3 * test.DimX / 100, 0, test.DimY, 2);
+ImaginaryPotential<WF_2D> imagpotr = new ImaginaryPotential<WF_2D>("ImagPotRechts", 97 * test.DimX / 100, test.DimX, 0, test.DimY, 2);
+ImaginaryPotential<WF_2D> imagpoto = new ImaginaryPotential<WF_2D>("ImagPotLinks", 3 * test.DimX / 100, 97 * test.DimX / 100, 97 * test.DimY / 100, test.DimY, 2);
+ImaginaryPotential<WF_2D> imagpotu = new ImaginaryPotential<WF_2D>("ImagPotRechts", 3 * test.DimX / 100, 97 * test.DimX / 100, 0, 3 * test.DimY / 100, 2);
 hamlist.Add(ham);
 hamlist.Add(imagpotl);
 hamlist.Add(imagpotr);
 hamlist.Add(imagpoto);
 hamlist.Add(imagpotu);
-hamlist.Add(afpot1);
-hamlist.Add(afpot2);
+//hamlist.Add(afpot1);
+//hamlist.Add(afpot2);
 //hamlist.Add(pot1);
 //hamlist.Add(pot2);
 
 //U_T<WF_1D> ze = new U_T<WF_1D>(0.5);
 U_T<WF_2D> ze = new U_T<WF_2D>(0.5);
 
-FFMpeg_ImageToVideo recorder = new FFMpeg_ImageToVideo(@"C:\ffmpeg\bin\", @"C:\ffmpeg\out.mp4", 800, 600, 50);
+FFMpeg_ImageToVideo recorder = new(@"C:\ffmpeg\bin\", Environment.CurrentDirectory + Path.DirectorySeparatorChar + "out.mp4", 800, 600, 50);
 
 var erg = test.GetImage(800, 600);
 recorder.AddNextImage(erg);
-
+test.AddCabExitAuto();
+var cabimg = test.GetCabExitImage(800, 600);
+cabimg.Save(Environment.CurrentDirectory + Path.DirectorySeparatorChar + "CAB_Exits.png");
+CabLogger logger = new();
 for (int i = 0; i < 200; i++)
 {
     DateTime start = DateTime.Now;
     test = ze.Do(test, hamlist);
+    logger.AddCab(i * ze.t_STEP, test);
     Console.WriteLine("Dauer Sek: " + (DateTime.Now - start).TotalSeconds);
     Console.WriteLine("Norm: " + test.Norm());
     if (i % 2 == 0)
@@ -60,4 +64,11 @@ for (int i = 0; i < 200; i++)
         recorder.AddNextImage(erg);
     }
 }
+var img = logger.GetImage(800, 600);
+for (int i1 = 0; i1 < img.Count; i1++)
+{
+    System.Drawing.Image? im = img[i1];
+    im.Save(Environment.CurrentDirectory + Path.DirectorySeparatorChar + "CAB_" + i1 + ".png");
+}
+
 recorder.Dispose();
