a4d70901cebfb2d4d444173bc5eee953acbc10ee
Michael Joppe <michael@JOPPE.intern>
2024-03-07 22:02:06 +0100

    Cab Logger für Cab Werte

---
+++ JScience/Physik/Simulationen/Wavefunctions/TimeEvolution/VarTypes/CabEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.VarTypes
+{
+    public struct CabEntry
+    {
+        public string ExitName { get; set; }
+        public double t { get; set; }
+        public Complex cab { get; set; }
+    }
+}
