07bd52ec40aa9e98dd0444fa40951e2f364b96ee
Michael Joppe <michael@JOPPE.intern>
2024-04-06 22:53:24 +0200

    Einträge für Sab analog zu Cab

---
+++ JScience/Physik/Simulationen/Wavefunctions/TimeEvolution/VarTypes/SabEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace JScience.Physik.Simulationen.Wavefunctions.TimeEvolution.VarTypes
+{
+    public struct SabEntry
+    {
+        public string ExitName { get; set; }
+        public double v { get; set; }
+        public Complex sab { get; set; }
+
+        public double GetSab2() => (Complex.Conjugate(sab) * sab).Real;
+    }
+}
