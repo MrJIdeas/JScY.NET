b77977f3a356257a4bf30f0d20e2c3dcc266e79e
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-01 02:26:11 +0100

    nur neue Berechnung, wenn notwendig

--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/AF_Potential.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/AF_Potential.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using HarfBuzzSharp;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
@@ -24,22 +26,34 @@ namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentia
         }
 
         public int Blocksize { get; private set; }
+        private Dictionary<string, int> LastDims { get; set; }
+        private Dictionary<int, int> fieldpos = [];
 
         public override IWavefunction HPsi(IWavefunction psi)
         {
             IWavefunction psiV = (IWavefunction)Activator.CreateInstance(psi.GetType(), psi.WFInfo, psi.CalcMethod);
             var dims = psi.GetDimensionLength();
-            var dimX = dims["x"];
-            var dimY = dims.ContainsKey("y") ? dims["y"] : 1;
 
-            for (int i = xStart; i < xEnd; i++)
-                for (int j = yStart; j < yEnd; j++)
-                    for (int k = zStart; k < zEnd; k++)
-                    {
-                        int idx = i + (j + k * dimY) * dimX;
-                        int sign = (i - xStart) % Blocksize % 2 == (j - yStart) % Blocksize % 2 ? 1 : -1;
-                        psiV.SetField(sign * psi.field[idx], i, j, k);
-                    }
+            if (LastDims == null || dims.Count != LastDims.Count)
+            {
+                fieldpos.Clear();
+                var dimX = dims["x"];
+                var dimY = dims.ContainsKey("y") ? dims["y"] : 1;
+
+                for (int i = xStart; i < xEnd; i++)
+                    for (int j = yStart; j < yEnd; j++)
+                        for (int k = zStart; k < zEnd; k++)
+                        {
+                            int idx = i + (j + k * dimY) * dimX;
+                            int sign = (i - xStart) % Blocksize % 2 == (j - yStart) % Blocksize % 2 ? 1 : -1;
+                            fieldpos.Add(idx, sign);
+                        }
+                LastDims = dims;
+            }
+            Parallel.ForEach(fieldpos, number =>
+            {
+                psiV.field[number.Key] = number.Value * psi.field[number.Key];
+            });
             return psiV * Potential;
         }
     }
