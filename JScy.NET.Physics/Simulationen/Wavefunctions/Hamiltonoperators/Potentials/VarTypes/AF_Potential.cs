5b8283c53f872ec12b31b14b48dd2db6d3ec7768
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-01 02:02:18 +0100

    AF Potentiale nicht mehr generisch

--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/AF_Potential.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/AF_Potential.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using HarfBuzzSharp;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
+using ScottPlot;
 
 namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes
 {
-    public class AF_Potential<T> : BlockPotential, IAFBarrier where T : IWF_2D
+    public class AF_Potential : BlockPotential, IAFBarrier
     {
         public AF_Potential(string name, int xSTART, int xEND, double Vmax, int Blocksize) : base(name, xSTART, xEND, Vmax)
         {
@@ -16,18 +18,31 @@ namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentia
             Blocksize = blocksize;
         }
 
+        public AF_Potential(string name, int xSTART, int ySTART, int zStart, int xEND, int yEND, int zEND, double Vmax, int blocksize) : base(name, xSTART, zStart, ySTART, xEND, yEND, zEND, Vmax)
+        {
+            Blocksize = blocksize;
+        }
+
         public int Blocksize { get; private set; }
 
         public override IWavefunction HPsi(IWavefunction psi)
         {
-            IWF_2D psiV = (IWF_2D)Activator.CreateInstance(psi.GetType(), psi.WFInfo, psi.CalcMethod);
+            IWavefunction psiV = (IWavefunction)Activator.CreateInstance(psi.GetType(), psi.WFInfo, psi.CalcMethod);
+            var dims = psi.GetDimensionLength();
+            var dimX = dims["x"];
+            var dimY = dims.ContainsKey("y") ? dims["y"] : 1;
+
             for (int i = xStart; i < xEnd; i++)
                 for (int j = yStart; j < yEnd; j++)
-                    if ((i - xStart) % Blocksize % 2 == (j - yStart) % Blocksize % 2)
-                        psiV.SetField(i, j, ((IWF_2D)psi)[i, j]);
-                    else
-                        psiV.SetField(i, j, -((IWF_2D)psi)[i, j]);
-            return (IWavefunction)(psiV * Potential);
+                    for (int k = zStart; k < zEnd; k++)
+                    {
+                        var idx = i + j * dimX + k * dimY * dimX;
+                        if ((i - xStart) % Blocksize % 2 == (j - yStart) % Blocksize % 2)
+                            psiV.SetField(psi.field[idx], i, j, k);
+                        else
+                            psiV.SetField(-psi.field[idx], i, j, k);
+                    }
+            return psiV * Potential;
         }
     }
 }
