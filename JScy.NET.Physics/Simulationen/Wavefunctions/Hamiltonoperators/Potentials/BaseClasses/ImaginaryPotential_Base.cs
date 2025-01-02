fbc0015786a34a9c9fa488408ca3dbe202e3d4b9
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-02 20:30:47 +0100

    Korrektur keine Verstärkung

--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/ImaginaryPotential_Base.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/ImaginaryPotential_Base.cs
@@ -5,6 +5,6 @@ namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentia
 {
     public abstract class ImaginaryPotential_Base(string name, double Vmax, int xSTART, int xEND, int ySTART, int yEND, int zSTART, int zEND) : Potential_Base(name, Vmax, xSTART, xEND, ySTART, yEND, zSTART, zEND), IPotential
     {
-        protected Complex ImagPotential => -Potential * Complex.ImaginaryOne;
+        protected Complex ImagPotential => Potential * Complex.ImaginaryOne;
     }
 }
