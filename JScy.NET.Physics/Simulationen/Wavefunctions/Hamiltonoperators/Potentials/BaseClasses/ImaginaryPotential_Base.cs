37ee6c38997aa081e0bade8e6efde2194d51fe5a
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-02 18:51:10 +0100

    Wert in Ableitungen verfügbar machen

--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/ImaginaryPotential_Base.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/ImaginaryPotential_Base.cs
@@ -1,15 +1,12 @@
 ﻿using System.Numerics;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
-using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
 
 namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.BaseClasses
 {
     public abstract class ImaginaryPotential_Base : Potential_Base, IPotential
     {
-        private Complex ImagPotential { get; set; }
+        protected Complex ImagPotential { get; private set; }
 
         protected ImaginaryPotential_Base(string name, double Vmax, int xSTART, int xEND, int ySTART, int yEND, int zSTART, int zEND) : base(name, Vmax, xSTART, xEND, ySTART, yEND, zSTART, zEND) => ImagPotential = Potential * Complex.ImaginaryOne;
-
-        public override IWavefunction HPsi(ref IWavefunction psi) => (getPsiV(ref psi) * ImagPotential);
     }
 }
