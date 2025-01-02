267df7fbdf6b20bc6df4fdc3bfc3979ee3a36f28
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-02 20:31:41 +0100

    Sammliung für alle statischen Potentiale
    nützlich, falls viele statischen Potentiale im Spiel sind - 1x rechen statt x-mal

---
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/VarTypes/PotentialCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.BaseClasses;
+using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
+using JScy.NET.Physics.Simulationen.Wavefunctions.VarTypes.StandardWF;
+
+namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.VarTypes
+{
+    public class PotentialCollection : Potential_Base
+    {
+        public PotentialCollection(string name, WFInfo wfinfo) : base(name, 0, 0, 0, 0, 0, 0, 0)
+        {
+            psiV = (IWavefunction)Activator.CreateInstance(wfinfo.Type, wfinfo);
+        }
+
+        public void MigratePotential(Potential_Base OPot)
+        {
+            psiV += OPot.psiV;
+        }
+    }
+}
