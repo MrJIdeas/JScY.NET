a56b9347ddd801ff743d3980e7bcfb0181bd58f2
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-02 17:38:20 +0100

    CodeMaid

--- JScy.NET.Physics/Simulationen/Wavefunctions/Analyse/Classes/Normlogger.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Analyse/Classes/Normlogger.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Numerics;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Analyse.Interfaces;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Analyse.VarTypes;
 using JScy.NET.Physics.Simulationen.Wavefunctions.VarTypes.Orbitale;
--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/Potential_Base.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/Potential_Base.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Numerics;
 using System.Threading.Tasks;
 using JScy.NET.Enums;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.BaseClasses;
@@ -8,24 +7,15 @@ using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
 
 namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.BaseClasses
 {
-    public abstract class Potential_Base : Hamilton_Base, IPotential
+    public abstract class Potential_Base(string name, double Vmax, int xSTART, int xEND, int ySTART, int yEND, int zSTART, int zEND) : Hamilton_Base, IPotential
     {
-        public string Name { get; private set; }
+        public string Name { get; private set; } = name;
 
-        public double Potential { get; private set; }
+        public double Potential { get; private set; } = Vmax;
 
-        public Tuple<int, int> limit_x { get; private set; }
-        public Tuple<int, int> limit_y { get; private set; }
-        public Tuple<int, int> limit_z { get; private set; }
-
-        public Potential_Base(string name, double Vmax, int xSTART, int xEND, int ySTART, int yEND, int zSTART, int zEND)
-        {
-            Name = name;
-            Potential = Vmax;
-            limit_x = new Tuple<int, int>(xSTART, xEND);
-            limit_y = new Tuple<int, int>(ySTART, yEND);
-            limit_z = new Tuple<int, int>(zSTART, zEND);
-        }
+        public Tuple<int, int> limit_x { get; private set; } = new Tuple<int, int>(xSTART, xEND);
+        public Tuple<int, int> limit_y { get; private set; } = new Tuple<int, int>(ySTART, yEND);
+        public Tuple<int, int> limit_z { get; private set; } = new Tuple<int, int>(zSTART, zEND);
 
         protected IWavefunction getPsiV(ref IWavefunction psi)
         {
