eb9b0e4882b635d3ba1d9678ccc3b5b9703a37be
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-02 19:59:52 +0100

    Plotten von Potentialen

--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/Potential_Base.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/Potential_Base.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using JScy.NET.Enums;
+using JScy.NET.Interfaces;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.BaseClasses;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.Interfaces;
 using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
+using ScottPlot;
 
 namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentials.BaseClasses
 {
-    public abstract class Potential_Base(string name, double Vmax, int xSTART, int xEND, int ySTART, int yEND, int zSTART, int zEND) : Hamilton_Base, IPotential
+    public abstract class Potential_Base(string name, double Vmax, int xSTART, int xEND, int ySTART, int yEND, int zSTART, int zEND) : Hamilton_Base, IPotential, IPlotter
     {
         public string Name { get; private set; } = name;
 
@@ -17,6 +21,8 @@ namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentia
         public Tuple<int, int> limit_y { get; private set; } = new Tuple<int, int>(ySTART, yEND);
         public Tuple<int, int> limit_z { get; private set; } = new Tuple<int, int>(zSTART, zEND);
         protected IWavefunction psiV { get; set; }
+
+        private readonly Plot myPlot_wf = new();
 
         protected virtual IWavefunction getPsiV(ref IWavefunction psi)
         {
@@ -58,10 +64,40 @@ namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentia
                         break;
                 }
             }
-
             return psiV;
         }
 
         public override IWavefunction HPsi(ref IWavefunction psi) => psi * getPsiV(ref psi);
+
+        public System.Drawing.Image GetImage(int width, int height)
+        {
+            if (psiV == null) return null;
+            myPlot_wf.Clear();
+            if (psiV.WFInfo.DimInfo.Dimensions == 1 && psiV is IWF_1D d)
+            {
+                var DimX = d.WFInfo.DimInfo.DimX;
+                List<double> x = [];
+                for (int i = 0; i < DimX; i++)
+                    x.Add(i);
+                List<double> y = [];
+                for (int i = 0; i < DimX; i++)
+                    y.Add(psiV.field[i].Magnitude);
+                myPlot_wf.Add.Bars(x, y);
+            }
+            else if (psiV.WFInfo.DimInfo.Dimensions == 2 && psiV is IWF_2D d1)
+            {
+                double[,] data = new double[d1.WFInfo.DimInfo.DimX, d1.WFInfo.DimInfo.DimY];
+                for (int i = 0; i < d1.WFInfo.DimInfo.DimX; i++)
+                    for (int j = 0; j < d1.WFInfo.DimInfo.DimY; j++)
+                        data[i, j] = d1[i, j].Magnitude;
+                var hm1 = myPlot_wf.Add.Heatmap(data);
+                hm1.Colormap = new ScottPlot.Colormaps.Turbo();
+                myPlot_wf.Add.ColorBar(hm1);
+            }
+            else
+                return null;
+            var img = System.Drawing.Image.FromStream(new MemoryStream(myPlot_wf.GetImage(width, height).GetImageBytes()));
+            return img;
+        }
     }
 }
