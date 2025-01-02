d86ff6901af0950c4913b8e93e4381abdb0e8ecb
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-02 16:37:44 +0100

    Codeoptimierung mit Koordinatenberechnen

--- JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/Potential_Base.cs
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/Potentials/BaseClasses/Potential_Base.cs
@@ -30,15 +30,41 @@ namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Potentia
         protected IWavefunction getPsiV(ref IWavefunction psi)
         {
             IWavefunction psiV = (IWavefunction)Activator.CreateInstance(psi.GetType(), psi.WFInfo);
+
             int dimYZ = psi.WFInfo.DimInfo.DimY * psi.WFInfo.DimInfo.DimZ;
-            int idx, i, j, k;
-            for (i = xStart; i < xEnd; i++)
-                for (j = yStart; j < yEnd; j++)
-                    for (k = zStart; k < zEnd; k++)
+
+            switch (psi.WFInfo.CalcMethod)
+            {
+                case ECalculationMethod.CPU:
+                    int idx, i, j, k;
+                    for (i = limit_x.Item1; i < limit_x.Item2; i++)
+                        for (j = limit_y.Item1; j < limit_y.Item2; j++)
+                            for (k = limit_z.Item1; k < limit_z.Item2; k++)
+                            {
+                                idx = dimYZ * k + psi.WFInfo.DimInfo.DimX * j + i;
+                                psiV.field[idx] = psi.field[idx];
+                            }
+
+                    break;
+
+                case ECalculationMethod.CPU_Multihreading:
+                case ECalculationMethod.OpenCL:
+                    var psi2 = psi;
+                    Parallel.For(limit_x.Item1, limit_x.Item2, i =>
                     {
-                        idx = dimYZ * k + psi.WFInfo.DimInfo.DimX * j + i;
-                        psiV.field[idx] = psi.field[idx];
-                    }
+                        int x = i % psi2.WFInfo.DimInfo.DimX;
+                        int y = (i / psi2.WFInfo.DimInfo.DimX) % psi2.WFInfo.DimInfo.DimY;
+                        int z = i / (psi2.WFInfo.DimInfo.DimX * psi2.WFInfo.DimInfo.DimY);
+
+                        if (x >= limit_x.Item1 && x < limit_x.Item2 &&
+                            y >= limit_y.Item1 && y < limit_y.Item2 &&
+                            z >= limit_z.Item1 && z < limit_z.Item2)
+                        {
+                            psiV.field[i] = psi2.field[i];
+                        }
+                    });
+                    break;
+            }
             return psiV;
         }
 
