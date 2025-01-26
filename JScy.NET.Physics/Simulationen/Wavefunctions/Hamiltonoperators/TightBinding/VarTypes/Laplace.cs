30007b8513187823b89d41d5a1af64b859f5592a
Michael Joppe <michael.joppe@mj-ideen.de>
2025-01-26 22:42:18 +0100

    einfacher Laplaceoperator für Wasserwellen

---
+++ JScy.NET.Physics/Simulationen/Wavefunctions/Hamiltonoperators/TightBinding/VarTypes/Laplace.cs
@@ -0,0 +1,9 @@
+﻿using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
+
+namespace JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.TightBinding.VarTypes
+{
+    public class Laplace(double coupling) : TightBinding(coupling)
+    {
+        public override IWavefunction HPsi(ref IWavefunction psi) => (-t_Hopping * 2 * psi.WFInfo.DimInfo.Dimensions) * psi - base.HPsi(ref psi);
+    }
+}
