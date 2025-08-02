8177fa2b45b024f2eb3d33e79b2760ea82040439
Michael Joppe <147102320+MrJIdeas@users.noreply.github.com>
2025-08-02 10:19:12 +0200

    Basisklasse zur Kalkulation einer Eigenzeit

---
+++ JScy.NET.Physics/Simulationen/Wavefunctions/TimeEvolution/Classes/U_T_eigen.cs
@@ -0,0 +1,21 @@
+﻿using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
+using JScy.NET.Physics.Simulationen.Wavefunctions.TimeEvolution.Interfaces;
+using JScy.NET.Physics.Simulationen.Wavefunctions.VarTypes.Orbitale;
+using System.Collections.Generic;
+
+namespace JScy.NET.Physics.Simulationen.Wavefunctions.TimeEvolution.Classes
+{
+    public abstract class U_T_eigen() : IU_T_eigenzeit
+    {
+        private readonly U_T_flex u_T_Flex = new();
+        public double LastTAU { get; protected set; }
+
+        protected abstract void CalcTAU(ref Orbital WF, List<IHamilton> Hamiltons);
+
+        public Orbital Do(ref Orbital WF, List<IHamilton> Hamiltons)
+        {
+            CalcTAU(ref WF, Hamiltons);
+            return u_T_Flex.Do(ref WF, Hamiltons, LastTAU);
+        }
+    }
+}
