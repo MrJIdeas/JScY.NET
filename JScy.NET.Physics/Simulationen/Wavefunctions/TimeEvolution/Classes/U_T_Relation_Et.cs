e71e1d8d646b340e27d07bf36cb671b626b9e07c
Michael Joppe <147102320+MrJIdeas@users.noreply.github.com>
2025-08-02 10:19:26 +0200

    Nutzung der Unschärferelation zur Bestimmung der Eigenzeit

---
+++ JScy.NET.Physics/Simulationen/Wavefunctions/TimeEvolution/Classes/U_T_Relation_Et.cs
@@ -0,0 +1,26 @@
+﻿using JScy.NET.Physics.Simulationen.Wavefunctions.Hamiltonoperators.Interfaces;
+using JScy.NET.Physics.Simulationen.Wavefunctions.Interfaces;
+using JScy.NET.Physics.Simulationen.Wavefunctions.VarTypes.Orbitale;
+using System;
+using System.Collections.Generic;
+
+namespace JScy.NET.Physics.Simulationen.Wavefunctions.TimeEvolution.Classes
+{
+    public class U_T_Relation_Et : U_T_eigen
+    {
+        protected override void CalcTAU(ref Orbital WF, List<IHamilton> Hamiltons)
+        {
+            double energy = 0d, vari = 0d;
+            IWavefunction wf1 = WF.WF;
+            foreach (var ham in Hamiltons)
+            {
+                IWavefunction hampsi = ham.HPsi(ref wf1);
+                IWavefunction hampsi2 = ham.HPsi(ref hampsi);
+                energy += hampsi.Norm();
+                vari += hampsi2.Norm();
+            }
+            double varianz = vari - Math.Pow(energy, 2);
+            LastTAU = 1 / Math.Sqrt(varianz);
+        }
+    }
+}
